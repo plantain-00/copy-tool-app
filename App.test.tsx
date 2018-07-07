b35c9ab07dd2e884d39b734db29c852e2db7b27b
york yao <yaoao12306@outlook.com>
2018-07-07 14:17:28 +0800

    feat: remove test

--- App.test.tsx
+++
@@ -1,6 +0,0 @@
-it('renders without crashing', () => {
-  //   const app = renderer.create(<App />);
-  //   const rendered = app.toJSON();
-  //   expect(rendered).toBeTruthy();
-  //   app.unmount();
-})
